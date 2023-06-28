9700cdc4784f2589cc24997b4d5506e4f6af8df4
Harishrajofficial <harishofficial3954@gmail.com>
2023-06-28 12:23:21 +0530

    some changes

--- register.php
+++ register.php
@@ -1,21 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width-device-width,initial-scale=1.0" />
-    <meta http-equiv="X-UA-Compatible" content="sie-edge" />
-    <link
-      href="https://unpkg.com/ionicons@4.2.2/dist/css/ionicons.min.css"
-      rel="stylesheet"
-    />
-    <link rel="stylesheet" href="css/reg.css" />
-    <title>Slider Form</title>
-    <style>
-			.error {color: #FF0000;}
-		</style>
-  </head>
-
-  <?php
+<?php
   
   session_start();
 
@@ -128,7 +111,24 @@ if (isset($_POST['sub'])) {
 
 ?>
 
-  <body>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width-device-width,initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="sie-edge" />
+    <link
+      href="https://unpkg.com/ionicons@4.2.2/dist/css/ionicons.min.css"
+      rel="stylesheet"
+    />
+    <link rel="stylesheet" href="css/reg.css" />
+    <title>Slider Form</title>
+    <style>
+			.error {color: #FF0000;}
+		</style>
+  </head>
+
+ <body>
   
   <div class="container" id="container">
 
